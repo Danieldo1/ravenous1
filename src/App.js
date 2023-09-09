5353a8d9f8819a6ded699ed36027cb72fd0ceb98
Dan <daniel.speranskiy@live.com>
2023-09-09 18:26:22 +0400

    Finished part 2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import BusinessList from "./BusinessList/BusinessList";
 import "./App.css";
 import {useState} from 'react'
 
-const business = {
+const business = [{
   imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
   name: "MarginOtto Pizzeria",
   address: "1010 Paddington Way",
@@ -14,10 +14,24 @@ const business = {
   category: "Italian",
   rating: 4.5,
   reviewCount: 90,
-};
+},
+{
+  imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
+  name: "MarginOtto Pizzeria",
+  address: "1010 Paddington Way",
+  city: "Flavortown",
+  state: "NY",
+  zipCode: "10101",
+  category: "Italian",
+  rating: 4.5,
+  reviewCount: 90,
+},
+];
 
 function App() {
 const [arr,setArr] = useState([])
+
+
 
   return (
     <div >
